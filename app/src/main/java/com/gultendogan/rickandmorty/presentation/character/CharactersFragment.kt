4efcd27e25a858d9bd4495459e46a1a1c3b44e3f
gulten27 <ggdogann00@gmail.com>
2022-10-26 19:39:08 +0300

    commit

--- app/src/main/java/com/gultendogan/rickandmorty/presentation/character/CharactersFragment.kt
+++ app/src/main/java/com/gultendogan/rickandmorty/presentation/character/CharactersFragment.kt
@@ -2,13 +2,21 @@ package com.gultendogan.rickandmorty.presentation.character
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import androidx.transition.AutoTransition
+import androidx.transition.TransitionManager
 import com.gultendogan.rickandmorty.R
+import com.gultendogan.rickandmorty.databinding.CharacterRowLayoutBinding
 import com.gultendogan.rickandmorty.databinding.FragmentCharactersBinding
 import com.gultendogan.rickandmorty.presentation.adapter.CharacterAdapter
+import com.gultendogan.rickandmorty.utils.dpToPx
 import com.gultendogan.rickandmorty.utils.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
@@ -17,15 +25,12 @@ import kotlinx.coroutines.flow.collectLatest
 class CharactersFragment : Fragment(R.layout.fragment_characters) {
     private val binding by viewBinding (FragmentCharactersBinding::bind)
     private val viewModel: CharacterViewModel by viewModels()
-    private lateinit var mAdapter: CharacterAdapter
+    private val mAdapter by lazy { CharacterAdapter() }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.rvCharacter.layoutManager =
-            StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
-        mAdapter = CharacterAdapter()
-        binding.rvCharacter.adapter = mAdapter
+        binding.characterAdapter = mAdapter
 
         lifecycleScope.launchWhenStarted {
             viewModel.characterList.collectLatest {
