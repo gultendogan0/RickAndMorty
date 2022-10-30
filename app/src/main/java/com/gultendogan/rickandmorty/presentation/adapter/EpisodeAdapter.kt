06c93df343370420a1519b24b8776461822eb8a1
gulten27 <ggdogann00@gmail.com>
2022-10-30 19:35:22 +0300

    commit

--- app/src/main/java/com/gultendogan/rickandmorty/presentation/adapter/EpisodeAdapter.kt
+++ app/src/main/java/com/gultendogan/rickandmorty/presentation/adapter/EpisodeAdapter.kt
@@ -2,9 +2,11 @@ package com.gultendogan.rickandmorty.presentation.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.gultendogan.rickandmorty.R
 import com.gultendogan.rickandmorty.databinding.EpisodeRowLayoutBinding
 import com.gultendogan.rickandmorty.domain.uimodel.EpisodeUIModel
 import com.gultendogan.rickandmorty.presentation.episode.EpisodeViewModel
@@ -14,21 +16,26 @@ class EpisodeAdapter(var viewModel: EpisodeViewModel) :
     class EpisodeViewHolder(var binding: EpisodeRowLayoutBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
+        fun bind(episode: EpisodeUIModel) {
+            binding.episode = episode
+        }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EpisodeViewHolder {
-        val view = EpisodeRowLayoutBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val view: EpisodeRowLayoutBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.episode_row_layout, parent, false
+        )
         return EpisodeViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: EpisodeViewHolder, position: Int) {
         val currentEpisode = getItem(position)!!
+        holder.bind(currentEpisode)
         val hb = holder.binding
         val mContext = hb.root.context
 
-        hb.tvRowEpisode.text = currentEpisode.episode
-        hb.tvRowName.text = currentEpisode.name
-        hb.tvRowAirDate.text = currentEpisode.airDate
     }
 
     class DiffUtilCallBack : DiffUtil.ItemCallback<EpisodeUIModel>() {
